1c2dc5757d92f170c7eb7c11baf7fcae3db25289
Brian Tieu <btieu@princeton.edu>
2023-04-10 11:37:08 -0400

    add design and new award

--- js/scripts.js
+++ js/scripts.js
@@ -66,3 +66,30 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
+// init swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    autoplay: {
+        delay: 5000,
+    },
+  });
