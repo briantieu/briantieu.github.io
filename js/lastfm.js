3d1ca661d62269a4610662603dce232b06de4e2c
Brian Tieu <btieu@princeton.edu>
2023-07-08 16:50:24 -0400

    http to https

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,5 +1,5 @@
 // get most recently listened track
-fetch('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=briantieu&api_key=9ba3923ace92512bcc5ea8a4eab44854&format=json&limit=1')
+fetch('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=briantieu&api_key=9ba3923ace92512bcc5ea8a4eab44854&format=json&limit=1')
 .then(res=>res.json())
 .then(json=> {
     const artist = json.recenttracks.track[0].artist['#text'];
